62ac76c731180ae23f18c52f9735d385b4199eff
Dmitriy Konstantinov <warqueq@gmail.com>
2017-03-24 18:45:26 +0200

    added pull of docker image into recipe

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,6 +7,10 @@ docker_service 'default' do
   action [:create, :start]
 end
 
+docker_image 'dmkonst/fun_with_jenkins' do
+  action :pull
+end
+
 docker_container 'test_jenkins_image' do
   repo 'dmkonst/fun_with_jenkins'
   port '8080:8080'
