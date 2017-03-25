4ca6b2319a404630580ca987f7c3e0420c099393
Dmitriy Konstantinov <warqueq@gmail.com>
2017-03-26 01:37:14 +0200

    fixed berks missing issue

--- cookbooks/chef_docker/provision.sh
+++ cookbooks/chef_docker/provision.sh
@@ -2,9 +2,19 @@
 
 curl -L https://www.opscode.com/chef/install.sh | sudo bash
 
-cd /home/ubuntu/cookbooks/chef_docker
-sudo berks vendor /home/ubuntu/cookbooks/
 
-cd /home/ubuntu
+echo "setting up berkshelf"
+sudo apt-get update
+sudo apt-get install gcc make -y
+
+sudo /opt/chef/embedded/bin/gem install berkshelf --no-ri --no-rdoc
+
+
+echo "getting docker cookbook wth from supermarket"
+cd /home/ubuntu/cookbooks/chef_docker
+sudo /opt/chef/embedded/bin/berks vendor /home/ubuntu/cookbooks/
+
+
 echo "Provisioning with chef-zero"
+cd /home/ubuntu
 sudo chef-client -z -o chef_docker
